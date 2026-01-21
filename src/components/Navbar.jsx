53d3bf94b326da823591ca7330ae8f1b62be06c1
unknown <karnatisriram2006@gmail.com>
2026-01-21 18:35:13 +0530

    working test for tailwind

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
+const Navbar = () => {  
+    const [active,setactive]=useState('');
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link to="/" className="flex items-center gap-2" onClick={()=>{
+          setactive("");
+          window.scrollTo(0,0);
+        }}>
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
+          <p className="text-white text-[18px] font-bold cursor-pointer">Sriram <span className="sm:block hidden">|Karnati</span></p>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
